508d8a2500855b606e5fd38dd1d562bb8d1bb0d6
Tsatsch <sshsmnv2000@gmail.com>
2024-05-25 12:10:49 +0200

    header & footer setup

---
+++ frontend/schufher/src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="footer">
+      <div class="centered-content">
+        <p class="white-p">Greetings from TBC &#128525 Have a gday! &#128073 &#128072</p>
+      </div>
+    </div>
+  </template>
+  
+  <style scoped>
+  .white-p {
+    color: white;
+    font-weight: 400;
+  }
+  
+  .centered-content {
+    display: flex;
+    align-items: center;
+    height: 100%;
+  }
+  
+  .footer {
+    background-color: var(--color-dark-blue);
+    color: white;
+    text-align: left;
+    padding-left: 20px;
+    padding-top: 10px;
+    padding-bottom: 5px;
+    font-size: 16px;
+    width: 100%;
+    box-sizing: border-box;
+    height: 50px;
+  }
+  </style>
---
+++ frontend/schufher/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="app-container">
+      <Header />
+      <div class="content">
+        <!-- Add your content here -->
+      </div>
+      <Footer />
+    </div>
+  </template>
+  
+  <script setup>
+  import Header from '@/components/Header.vue'
+  import Footer from '@/components/Footer.vue'
+  </script>
+  
+  <style scoped>
+  .app-container {
+    display: flex;
+    flex-direction: column;
+    height: 95vh;
+  }
+  
+  .content {
+    flex: 1;
+    overflow-y: auto;
+  }
+  </style>
+  
